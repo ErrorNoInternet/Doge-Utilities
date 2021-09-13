c99c41c7ee3bada2aefc533b9c9f21ed3894954f
ErrorNoInternet <error.nointernet@gmail.com>
2021-09-13 18:42:26 +0800

    Fix bugs

--- functions.py
+++ functions.py
@@ -333,11 +333,9 @@ async def testsCommand(message, prefix):
                 for i in range(200):
                     process = psutil.Process(os.getpid())
                     memoryUsage = process.memory_info().rss / 1000000
-                    if memoryUsage > 100 and memoryUsage < 250:
-                        description = description.replace(test, test.replace(":grey_question:", ":yellow_square:")); continue
-                    if memoryUsage >= 250.0:
+                    if memoryUsage >= 250:
                         description = description.replace(test, test.replace(":grey_question:", ":orange_square:")); continue
-                    if round(psutil.cpu_percent()) >= 80 or (client.latency * 1000) > 800:
+                    if round(psutil.cpu_percent()) >= 80 or (client.latency * 1000) >= 800:
                         description = description.replace(test, test.replace(":grey_question:", ":yellow_square:")); continue
 
                     description = description.replace(test, test.replace(":grey_question:", ":green_square:"))
