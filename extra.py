7862d33d621a2a1c0b2217a18b6288d1979306ab
ErrorNoInternet <error.nointernet@gmail.com>
2021-09-16 18:14:17 +0800

    Fix indentation

--- extra.py
+++ extra.py
@@ -1,9 +1,10 @@
+import string
 import discord
 import variables
 import functions
 
 async def autoCount(channelID):
-	import string; digits = str(string.digits)
+	digits = str(string.digits)
 	for guild in functions.client.guilds:
 		for channel in guild.channels:
 			if channel.id == channelID:
@@ -20,10 +21,11 @@ async def autoCount(channelID):
 				await channel.send(number)
 
 async def postAnnouncement(title, text, mention=False):
-  for guild in functions.client.guilds:
-    if guild.id == 879662689708806154:
-      for channel in guild.channels:
-        if channel.id == 879665441545519134:
-          embed = discord.Embed(title=title, description=text, color=variables.embedColor)
-          message = await channel.send("<@&879665075642835006>" if mention else "", embed=embed)
-          await message.publish()
+    for guild in functions.client.guilds:
+        if guild.id == 879662689708806154:
+            for channel in guild.channels:
+                if channel.id == 879665441545519134:
+                    embed = discord.Embed(title=title, description=text, color=variables.embedColor)
+                    announcement = await channel.send("<@&879665075642835006>" if mention else "", embed=embed)
+                    await announcement.publish()
+
