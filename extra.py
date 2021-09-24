95a840e4db55bdff1b2b38113a09b95f8afa2700
ErrorNoInternet <error.nointernet@gmail.com>
2021-09-24 23:46:45 +0800

    Update files

--- extra.py
+++ extra.py
@@ -3,11 +3,11 @@ import disnake
 import variables
 import functions
 
-async def autoCount(channelID):
+async def auto_count(channel_id):
 	digits = str(string.digits)
 	for guild in functions.client.guilds:
 		for channel in guild.channels:
-			if channel.id == channelID:
+			if channel.id == channel_id:
 				lastMessage = await channel.history(limit=1).flatten()
 				lastMessage = lastMessage[0]; number = ""
 				for letter in lastMessage.content:
@@ -20,12 +20,12 @@ async def autoCount(channelID):
 					number = 0
 				await channel.send(number)
 
-async def postAnnouncement(title, text, mention=False):
+async def post_announcement(title, text, mention=False):
     for guild in functions.client.guilds:
         if guild.id == 879662689708806154:
             for channel in guild.channels:
                 if channel.id == 879665441545519134:
-                    embed = disnake.Embed(title=title, description=text, color=variables.embedColor)
+                    embed = disnake.Embed(title=title, description=text, color=variables.embed_color)
                     announcement = await channel.send("<@&879665075642835006>" if mention else "", embed=embed)
                     await announcement.publish()
 
