6d115863ac5f30f9071c40e0f24f704b7e06a85b
ErrorNoInternet <error.nointernet@gmail.com>
2021-10-03 20:46:48 +0800

    Fix bugs

--- functions.py
+++ functions.py
@@ -2271,8 +2271,8 @@ async def translate_command(
         translator = googletrans.Translator()
         result = translator.translate(text, dest=language.strip())
         embed = disnake.Embed(color=variables.embed_color)
-        embed.add_field(name=f"Original Text ({googletrans.LANGUAGES[result.src].title()})", value=text, inline=False)
-        embed.add_field(name=f"Translated Text ({googletrans.LANGUAGES[result.dest].title()})", value=result.text)
+        embed.add_field(name=f"Original Text ({googletrans.LANGUAGES[result.src.lower()].title()})", value=text, inline=False)
+        embed.add_field(name=f"Translated Text ({googletrans.LANGUAGES[result.dest.lower()].title()})", value=result.text)
         await interaction.edit_original_message(embed=embed)
         add_cooldown(interaction.author.id, "translate", 5)
     except Exception as error:
