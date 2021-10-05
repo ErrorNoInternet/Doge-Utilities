c904870e2290ff2480dd07017eeb3cc4012fe095
ErrorNoInternet <error.nointernet@gmail.com>
2021-10-05 22:55:55 +0800

    Add tictactoe command

--- core.py
+++ core.py
@@ -27,6 +27,7 @@ import simpleeval
 import contextlib
 import googletrans
 from PIL import Image
+from typing import List
 from dateutil import parser
 from disnake.ext import commands
 from disnake.ext.commands import Param
@@ -1455,29 +1456,108 @@ async def blacklist_remove_command(
     database["blacklist"] = json.dumps(current_users)
     await interaction.response.send_message("Successfully removed user from blacklist")
 
-@client.slash_command(name="fetch", description="Fetch something fun")
-async def fetch_command(_):
+@client.slash_command(name="game", description="Start a fun game")
+async def game_command(_):
     pass
 
-@fetch_command.sub_command(name="meme", description="Fetch a random meme from Reddit")
-async def meme_command(interaction):
-    await interaction.response.defer()
-    response = requests.get("https://meme-api.herokuapp.com/gimme").json()
-    description = f"Posted by **{response['author']}** in **{response['subreddit']}** (**{response['ups']}** upvotes)"
-    embed = disnake.Embed(title=response["title"], url=response["postLink"], description=description, color=variables.embed_color)
-    embed.set_image(url=response["url"])
-    await interaction.edit_original_message(embed=embed)
-    add_cooldown(interaction.author.id, "meme", 3)
+@game_command.sub_command(name="tictactoe", description="Start a TicTacToe game")
+async def tictactoe_command(interaction):
+    class TicTacToeButton(disnake.ui.Button['TicTacToe']):
+        def __init__(self, x: int, y: int):
+            super().__init__(style=disnake.ButtonStyle.secondary, label='\u200b', row=y)
+            self.x = x
+            self.y = y
 
-@fetch_command.sub_command(name="joke", description="Fetch a random joke")
-async def joke_command(interaction):
-    await interaction.response.defer()
-    response = requests.get("http://random-joke-api.herokuapp.com/random").json()
-    embed = disnake.Embed(description=f"Here's a `{response['type']}` joke:\n{response['setup']} **{response['punchline']}**", color=variables.embed_color)
-    await interaction.edit_original_message(embed=embed)
-    add_cooldown(interaction.author.id, "joke", 3)
+        async def callback(self, interaction: disnake.MessageInteraction):
+            assert self.view is not None
+            view: TicTacToe = self.view
+            state = view.board[self.y][self.x]
+            if state in (view.X, view.O):
+                return
 
-@fetch_command.sub_command(name="trivia", description="Fetch a trivia question")
+            if view.current_player == view.X:
+                self.style = disnake.ButtonStyle.danger
+                self.label = 'X'
+                self.disabled = True
+                view.board[self.y][self.x] = view.X
+                view.current_player = view.O
+                content = "It is now O's turn"
+            else:
+                self.style = disnake.ButtonStyle.success
+                self.label = 'O'
+                self.disabled = True
+                view.board[self.y][self.x] = view.O
+                view.current_player = view.X
+                content = "It is now X's turn"
+
+            winner = view.check_board_winner()
+            if winner is not None:
+                if winner == view.X:
+                    content = 'X won!'
+                elif winner == view.O:
+                    content = 'O won!'
+                else:
+                    content = "It's a tie!"
+
+                for child in view.children:
+                    child.disabled = True
+                view.stop()
+            await interaction.response.edit_message(content=content, view=view)
+
+    class TicTacToe(disnake.ui.View):
+        children: List[TicTacToeButton]
+        X = -1
+        O = 1
+        Tie = 2
+
+        def __init__(self):
+            super().__init__()
+            self.current_player = self.X
+            self.board = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+
+            for x in range(3):
+                for y in range(3):
+                    self.add_item(TicTacToeButton(x, y))
+
+        def check_board_winner(self):
+            for across in self.board:
+                value = sum(across)
+                if value == 3:
+                    return self.O
+                elif value == -3:
+                    return self.X
+
+            for line in range(3):
+                value = self.board[0][line] + self.board[1][line] + self.board[2][line]
+                if value == 3:
+                    return self.O
+                elif value == -3:
+                    return self.X
+
+            diag = self.board[0][2] + self.board[1][1] + self.board[2][0]
+            if diag == 3:
+                return self.O
+            elif diag == -3:
+                return self.X
+
+            diag = self.board[0][0] + self.board[1][1] + self.board[2][2]
+            if diag == 3:
+                return self.O
+            elif diag == -3:
+                return self.X
+
+            if all(i != 0 for row in self.board for i in row):
+                return self.Tie
+            return None
+
+    await interaction.response.send_message("Welcome to TicTacToe!", view=TicTacToe())
+    add_cooldown(interaction.author.id, "game", 3)
+
+@game_command.sub_command(name="trivia", description="Start a trivia game")
 async def trivia_command(interaction):
     await interaction.response.defer()
     url = f"https://opentdb.com/api.php?amount=1&type=multiple&category={random.randint(9, 33)}&difficulty={random.choice(['easy', 'medium', 'hard'])}"
@@ -1561,7 +1641,29 @@ async def trivia_command(interaction):
         color=variables.embed_color,
     )
     await interaction.edit_original_message(embed=embed, view=CommandView())
-    add_cooldown(interaction.author.id, "trivia", 10)
+    add_cooldown(interaction.author.id, "game", 3)
+
+@client.slash_command(name="fetch", description="Fetch something fun")
+async def fetch_command(_):
+    pass
+
+@fetch_command.sub_command(name="meme", description="Fetch a random meme from Reddit")
+async def meme_command(interaction):
+    await interaction.response.defer()
+    response = requests.get("https://meme-api.herokuapp.com/gimme").json()
+    description = f"Posted by **{response['author']}** in **{response['subreddit']}** (**{response['ups']}** upvotes)"
+    embed = disnake.Embed(title=response["title"], url=response["postLink"], description=description, color=variables.embed_color)
+    embed.set_image(url=response["url"])
+    await interaction.edit_original_message(embed=embed)
+    add_cooldown(interaction.author.id, "meme", 3)
+
+@fetch_command.sub_command(name="joke", description="Fetch a random joke")
+async def joke_command(interaction):
+    await interaction.response.defer()
+    response = requests.get("http://random-joke-api.herokuapp.com/random").json()
+    embed = disnake.Embed(description=f"Here's a `{response['type']}` joke:\n{response['setup']} **{response['punchline']}**", color=variables.embed_color)
+    await interaction.edit_original_message(embed=embed)
+    add_cooldown(interaction.author.id, "fetch", 3)
 
 @fetch_command.sub_command(name="quote", description="Fetch an inspirational quote")
 async def quote_command(interaction):
@@ -1569,7 +1671,7 @@ async def quote_command(interaction):
     try:
         response = json.loads(requests.get("https://zenquotes.io/api/random").content)[0]
         await interaction.edit_original_message(content=f'"{response["q"]}"\n\n\t\t\t\t**- {response["a"]}**')
-        add_cooldown(interaction.author.id, "quote", 5)
+        add_cooldown(interaction.author.id, "fetch", 3)
     except:
         await interaction.edit_original_message(content="Unable to fetch quote")
 
