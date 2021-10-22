a58ec87572bf72d3b01207851b3147dc1a48773b
ErrorNoInternet <error.nointernet@gmail.com>
2021-10-22 16:40:34 +0800

    Fix bugs

--- static/dashboard.js
+++ static/dashboard.js
@@ -20,26 +20,6 @@ function raidProtection(token, data) {
 	}
 }
 
-function toggleSnipe(token, data) {
-	var request = new XMLHttpRequest()
-	request.open("GET", `(website)/web/api/snipe/${token}/${data}`)
-	request.send()
-	request.onreadystatechange = (changeEvent) => {
-		text = request.responseText
-		if (text.includes("ratelimit")) {
-			alert("You are being ratelimited!")
-			return
-		}
-		var currentStatus = "Disabled"
-		if (text == "1") {
-			currentStatus = "Enabled"
-		}
-		var button = document.getElementById("snipe-button." + data)
-		button.innerHTML = "Message Snipe: " + currentStatus
-		button.style.background = colors[text]
-	}
-}
-
 function toggleFilter(token, name, data) {
 	var request = new XMLHttpRequest()
 	request.open("GET", `(website)/web/api/filter/${token}/${name}/${data}`)
