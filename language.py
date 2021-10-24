6128d66ffc5633c94b9b1c59271a00207818ae27
ErrorNoInternet <error.nointernet@gmail.com>
2021-10-24 12:30:38 +0800

    Fix bugs

--- language.py
+++ language.py
@@ -4,12 +4,14 @@ data = {
         "not_command_sender": "You are not the sender of that command!",
         "vote_message": "Thank you for voting for Doge Utilities!",
         "banned_message": "You are banned from using Doge Utilities!",
+        "error_message": "Oops! Doge Utilities has ran into an error...",
     },
     "zh-cn": {
         "no_permission": "你没有权限用这个指令!",
         "not_command_sender": "这个指令不是你发的!",
         "vote_message": "谢谢你给我投票!",
         "banned_message": "你已被禁止使用Doge Utilities!",
+        "error_message": "很抱歉，Doge Utilities出错了。。。",
     }
 }
 data["zh-tw"] = data["zh-cn"]
@@ -21,7 +23,7 @@ def get(language, key):
         language = "en"
     language_data = data[language]
     if key not in language_data:
-        return "Error"
+        return language_data["error_message"]
     else:
         return language_data[key]
 
