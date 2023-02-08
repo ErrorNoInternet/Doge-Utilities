18a35610684d6ef1310c1333c016d36ce0f7f4f3
ErrorNoInternet <error.nointernet@gmail.com>
2023-02-08 16:35:13 +0800

    Change colors back

--- variables.py
+++ variables.py
@@ -7,7 +7,7 @@ permission_override = bot_owners
 message_managers = bot_owners
 
 
-def embed_color(): return random.choice([0xC30F16, 0x0FC362])
+def embed_color(): return 0x20c2f6
 
 
 prefix = "="
